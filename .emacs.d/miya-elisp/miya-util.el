f63313f2038f490970c62f0c65ccabe34812c3f3
Shinya Miyazaki <sssx5559@gmail.com>
2016-12-14 07:21:06 +0900

    hippie-expand 優先順位変更

--- .emacs.d/miya-elisp/miya-util.el
+++ .emacs.d/miya-elisp/miya-util.el
@@ -369,14 +369,14 @@
 ;; 略語展開機能選択
 (setq hippie-expand-try-functions-list
 	  '(
-		try-complete-file-name-partially
-		try-complete-file-name
-;;		try-expand-all-abbrevs
-		try-expand-list
-		try-expand-line
 		try-expand-dabbrev
 		try-expand-dabbrev-all-buffers
 		try-expand-dabbrev-from-kill
+;;		try-expand-all-abbrevs
+		try-expand-list
+		try-expand-line
 		try-complete-lisp-symbol-partially
 		try-complete-lisp-symbol
+		try-complete-file-name-partially
+		try-complete-file-name
 		))
